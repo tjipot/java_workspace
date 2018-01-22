ef151589b63a7d9b2e3cba315786c1fd139ef32a
tjipot <tomjackinpot@163.com>
2018-01-22 18:41:57 +0800

    0122_SYN_O

--- java_demo/src/com/univesre/uuid/UUIDTester.java
+++ java_demo/src/com/univesre/uuid/UUIDTester.java
@@ -1,26 +1,111 @@
 package com.univesre.uuid;
 
-import java.util.Date;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
+/**
+ * @File: UUIDTester.java
+ * @Classname: UUIDTester
+ * @Description: 一个工厂类, 产生各种 Haoran 想要的数据, 噫哈!
+ * @Author: Haoran Ye
+ * @CreateDate: 18/1/22 17:03
+ * @ModificationHistory: WHAT WHEN WHO
+ **/
 public class UUIDTester {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        UUID uuid = UUID.randomUUID();
-        System.out.println(uuid);
+	public static void main(String[] args) {
+
+		// 产生UUID;
+		// UUID uuid = UUID.randomUUID();
+		// System.out.println(uuid);
+		// System.out.println(uuid.toString());
+		//	060d1093-e0cb-4608-b82f-57b90b98ab28
+		uuidStr2TxtFile("/Users/UNIVESRE/Desktop/uuidStr.txt", 10000);
+
+		// 产生时间;
+		// System.out.println(new Date().getTime());
+
+		// 测试6位兑换码是否重复: 看输出;
+		// verifyCouponCode("/Users/UNIVESRE/Desktop/pureSixChars.txt");
+
+	}
+
+	public static void uuidStr2TxtFile(String filePath, int uuidAmount) {
+		try {
+			// 产生UUID;
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < uuidAmount; i++) {
+				sb.append(UUID.randomUUID().toString() + "\n");    // "\n";
+			}
+			// 文件IO;
+			File file = new File(filePath);
+			if (!file.exists()) { file.createNewFile(); }
+			// System.out.println("sb.toString(): " + sb.toString());
+			PrintStream printStream = new PrintStream(new FileOutputStream(file));
+			printStream.print(String.valueOf(sb));
+			printStream.close();
+
+			// 注释掉的都不能把内容写入文件, 只能创建出txt文件;
+			// FileWriter fw = new FileWriter(file.getName());
+			// fw.write(String.valueOf(sb));
+
+			// BufferedWriter bw = new BufferedWriter(fw);
+//			PrintWriter pw = new PrintWriter(fw);
+//			pw.print(String.valueOf(sb));
+//			pw.close();
+			// System.out.print("String.valueOf(sb): " + String.valueOf(sb));
+			// bw.write(String.valueOf(sb));
+			// bw.write("Test");
+			// bw.flush();
+			// bw.close();
+			//file();
+			// fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 
-	    System.out.println(new Date().getTime());
-//		System.out.println(uuid.toString());
-    }
-    //	060d1093-e0cb-4608-b82f-57b90b98ab28
-    //	c0d16fcd-1c6e-47e4-891c-86f3020b0288
-    //	cfa87931-7a40-4e16-86b1-c35477f9d8d2
-    //	5fb338f6-1842-498c-8cc9-7dab6a781a6e
+	// 测试6位兑换码是否重复: 看输出;
+	public static void verifyCouponCode(String filePath) {
+		File file = new File(filePath);
+		Set<String> codeSet = new HashSet<String>();
+		String codeStr = text2String(file); // vq5giw|eh53cx| ... |8edwve|fhfq35;
+		// List<String> codeList = new ArrayList<String>();
+		// System.out.println("codeStr: " + codeStr);
+		String[] codeArray = codeStr.split(",");    // 用"|"就会错: codeArray's length: 349999;
+		// System.out.println("codeArray's length: " + codeArray.length);
+		int codeCountDup = 0;
+		int codeCountIndup = 0;
+		for (String str : codeArray) {
+			boolean b = codeSet.add(str);
+			if (!b) {
+				codeCountDup ++;
+				System.out.println(str);
+			} else {
+				codeCountIndup ++;
+			}
+		}
+		System.out.println("codeCountDup is: " + codeCountDup + ", codeCountIndup is: " + codeCountIndup + ".");
+	}
 
-    //	94f7b960-80c1-4ced-8004-4e66f56c6bb3
-
-    //	6e8feac4-991e-4922-a7e9-9d57b9722700
+	// 从文本文件中读取字串;
+	public static String text2String(File file) {
+		StringBuilder result = new StringBuilder();
+		String perfectFormat = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
+			String s = null;
+			while((s = br.readLine()) != null) {    // 使用readLine方法, 一次读一行;
+				result.append(s + ",");             // System.lineSeparator() => '\n';
+			}
+			perfectFormat = result.toString();
+			perfectFormat = perfectFormat.substring(0, result.length()-1);
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return perfectFormat;   // result.toString();
+	}
 
 }
