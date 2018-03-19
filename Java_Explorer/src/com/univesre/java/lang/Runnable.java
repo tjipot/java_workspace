8306ad1d5f65c14f46f70dce74f5f75e53c7ff63
tjipot <tomjackinpot@163.com>
2018-03-19 18:48:01 +0800

    0319_SYN_O

---
+++ Java_Explorer/src/com/univesre/java/lang/Runnable.java
@@ -0,0 +1,18 @@
+package com.univesre.java.lang;
+
+/**
+ * Created by UNIVESRE on 18/3/19 18:31.
+ */
+public interface Runnable {
+
+	/**Fullname: java.lang.Runnable
+	 * Note: The Runnable interface should be implemented by any class
+	 * whose instances are intended to be executed "by a thread". The class must define
+	 * a method of no arguments called "run".
+	 *
+	 * void run(): When an object implementing interface "java.lang.Runnable" is used to create a thread,
+	 * "starting the thread" causes the object's run method to be called in that separately executing thread.
+	 */
+
+
+}
